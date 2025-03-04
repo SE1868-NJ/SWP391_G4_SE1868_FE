7387bbd7ec918a3ad28a4c635699fc058dad35d1
taitran <tai05112004@gmail.com>
2025-03-04 11:03:56 +0700

    fixAll

--- swp391_g4/src/components/header/Header.js
+++ swp391_g4/src/components/header/Header.js
@@ -4,7 +4,6 @@ import styles from "./Header.module.css";
 import { NavigationItem } from "./NavigationItem";
 import { Logo } from "./Logo";
 import { AuthButton } from "./AuthButton";
-import { useNavigate } from 'react-router-dom';
 
 export class Header extends React.Component {
   constructor(props) {
